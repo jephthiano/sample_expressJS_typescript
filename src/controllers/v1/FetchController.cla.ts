97846a9f4fd1b8d25797dbe94e129df16797a1d3
Oladejo Jephthah <jephthahooh@gmail.com>
2025-08-06 09:30:09 +0100

    Controller section compltely changed to typescript

--- src/controllers/v1/FetchController.cla.ts
+++ src/controllers/v1/FetchController.cla.ts
@@ -1,13 +1,14 @@
+import { Request, Response } from 'express';
 import BaseController from '#controller/BaseController.cla.js';
 import FetchService from '#service/v1/FetchService.cla.js';
 
 
 class FetchController extends BaseController{
-    static async authFetchData (id){
+    static async authFetchData (id: string){
         return await FetchService.authFetchData(id);
     }
 
-    static async appFetchData (req, res){
+    static async appFetchData (req: Request, res: Response){
         try{
             const response = await FetchService.appFetchData(req);
 
