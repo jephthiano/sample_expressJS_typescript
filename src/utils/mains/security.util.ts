2cc570f3b9b2a557616b68fe8295efa0ce60345b
Oladejo Jephthah <jephthahooh@gmail.com>
2025-08-10 09:39:08 +0100

    security utils converted to typescript

--- src/utils/mains/security.util.ts
+++ src/utils/mains/security.util.ts
@@ -3,51 +3,54 @@ import cryptoJS from 'crypto-js';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
 import { queueRehash } from '#queue/rehashQueue.js';
+import { getEnvorThrow } from './general.util.js';
 
-const key = process.env.ENC_KEY;
-const iv = process.env.ENC_IV;
-// const method = process.env.ENC_METHOD; // Encryption method
-const cost = process.env.HASH_COST;
+const key = getEnvorThrow("ENC_KEY");
+const iv = getEnvorThrow("ENC_IV");
+const cost = getEnvorThrow("HASH_COST");
+// const method = getEnvorThrow("ENC_METHOD"); // Encryption method
+
 const enc_array = ['general', 'token', 'receiving_medium'];
 
 // Hash password asynchronously
-const hashPassword = async (password) => await bcrypt.hash(password, 10);
+const hashPassword = async (password: string) => await bcrypt.hash(password, 10);
 
 // Verify password asynchronously
-const verifyPassword = async (plainPassword, hashedPassword, userId = null) => {
+const verifyPassword = async (plainPassword: string, hashedPassword: string, userId: null | string = null) => {
     const match = await bcrypt.compare(plainPassword, hashedPassword);
     if (!match) return false;
 
-    const currentRounds = parseInt(hashedPassword.split('$')[2], 10);
-    const needsRehash = currentRounds !== cost;
-
-    if (needsRehash && userId) {
-            // pass to queue job
-            queueRehash({userId, plainPassword });
-    }
+    // pass to queue job [if rehash is needed]
+    if (passwordNeedRehash(hashedPassword) && userId) queueRehash({userId, plainPassword });
 
     return true;
 };
 
+
+const passwordNeedRehash = (hashedPassword: string): boolean => {
+    const currentRounds: number = parseInt(hashedPassword.split('$')[2], 10); // split by $
+    return currentRounds !== parseInt(cost);
+}
+
 // Encrypt only if type exists in enc_array
-const selEncrypt = (data, type = 'general') => enc_array.includes(type) ? encrypt(data) : data;
+const selEncrypt = (data: string, type: string = 'general') => enc_array.includes(type) ? encrypt(data) : data;
 
 // Decrypt only if type exists in enc_array
-const selDecrypt = (data, type = 'general') => enc_array.includes(type) ? decrypt(data) : data;
+const selDecrypt = (data :string , type: string = 'general') => enc_array.includes(type) ? decrypt(data) : data;
 
-const encrypt = (data) => cryptoJS.AES.encrypt(
+const encrypt = (data: string) => cryptoJS.AES.encrypt(
     data,
     cryptoJS.enc.Utf8.parse(key),
     { iv: cryptoJS.enc.Utf8.parse(iv) }
 ).toString();
 
-const decrypt = (data) => cryptoJS.AES.decrypt(
+const decrypt = (data: string) => cryptoJS.AES.decrypt(
     data,
     cryptoJS.enc.Utf8.parse(key),
     { iv: cryptoJS.enc.Utf8.parse(iv) }
 ).toString(cryptoJS.enc.Utf8);
 
-const validateInput = (data, type) => {
+const validateInput = (data: string, type: string) => {
     switch (type) {
         case 'email': return validator.isEmail(data);
         case 'name': return validator.isAlpha(data) && validator.isLength(data, { min: 1, max: 50 });
@@ -58,13 +61,13 @@ const validateInput = (data, type) => {
     }
 };
 
-const validatePin = (pin) => validator.isNumeric(pin) && validator.isLength(pin, { min: 4, max: 4 }) && pin !== '1234';
+const validatePin = (pin: string) => validator.isNumeric(pin) && validator.isLength(pin, { min: 4, max: 4 }) && pin !== '1234';
 
-const validatePassword = (password) => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+_!@#$^&*.,?]).{8,}$/.test(password);
+const validatePassword = (password: string) => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+_!@#$^&*.,?]).{8,}$/.test(password);
 
 const generateUniqueToken = () => crypto.randomBytes(32).toString('hex');
 
-const generateUniqueId = (max) => crypto.randomInt(2, Number(`1${'0'.repeat(max)}`));
+const generateUniqueId = (max: number) => crypto.randomInt(2, Number(`1${'0'.repeat(max)}`));
 
 export {
     hashPassword,
